83a533bee7f633eb2a4e33a5364dc86e31291c3d
Ivy <114126870+i-vy-i@users.noreply.github.com>
2026-04-28 09:11:34 -0500

    Adjust layout properties

--- frontend/src/graphs/RadarPlot.jsx
+++ frontend/src/graphs/RadarPlot.jsx
@@ -50,10 +50,10 @@ export default function RadarPlot({ goalIndex = 0, figure: prefetchedFigure }) {
     ...figure.layout,
     autosize: true,
     width: undefined,
-    height: 540,
+    // height: 540,
     paper_bgcolor: "rgba(0,0,0,0)",
     plot_bgcolor: "rgba(0,0,0,0)",
-    margin: { t: 120, l: 80, r: 80, b: 100 },
+    margin: { t: 80, l: 60, r: 60, b: 60 },
   };
 
   return (
